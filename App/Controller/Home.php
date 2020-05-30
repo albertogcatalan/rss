0b76da39e24df985ebb1e531e6bfd3f8e486739a
Alberto González <albertogonzalezcatalan@gmail.com>
2020-05-30 17:26:13 +0200

    Improvements and proofs

--- App/Controller/Home.php
+++ App/Controller/Home.php
@@ -18,6 +18,30 @@ class Home extends Controller
      */
     public function indexAction()
     {
+
+        $url = 'http://www.eldiario.es/rss/section/10838';
+        /*$atom = \Feed::loadAtom($url);
+
+        d($atom);*/
+
+        //$rss = \Feed::loadRss($url);
+
+        $rss = simplexml_load_file($url);
+
+        d($rss);
+
+        echo 'Title: ', $rss->title;
+        echo 'Description: ', $rss->description;
+        echo 'Link: ', $rss->link;
+
+        foreach ($rss->item as $item) {
+            echo 'Title: ', $item->title;
+            echo 'Link: ', $item->link;
+            echo 'Timestamp: ', $item->timestamp;
+            echo 'Description ', $item->description;
+            echo 'HTML encoded content: ', $item->{'content:encoded'};
+        }
+
         $this->addTwigVars('helloWorld', 'Mi first page class -> '.$this->getContainer()->get('config')->val('app.STABLE_VERSION'));
         $this->render();
     }
